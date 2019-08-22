b67b25cc17030ce26719a49e5eae3a5e811f0e5f
Arnaud Rocher <arnaud.roche3@gmail.com>
2019-08-22 22:47:26 +0200

    Add module documentation

---
+++ devices/mcp23xxx/doc.go
@@ -0,0 +1,22 @@
+/*
+Package mcp23xxx controls a Microchip MCP23xxx series I/O Expander over I²C
+or SPI.
+
+Datasheets for supported devices
+
+MCP23008 / MCP23S08 :
+http://ww1.microchip.com/downloads/en/DeviceDoc/MCP23008-MCP23S08-Data-Sheet-20001919F.pdf
+
+MCP23009 / MCP23S09 :
+http://ww1.microchip.com/downloads/en/DeviceDoc/20002121C.pdf
+
+MCP23016 :
+http://ww1.microchip.com/downloads/en/DeviceDoc/20090C.pdf
+
+MCP23017 / MCP23S17 :
+http://ww1.microchip.com/downloads/en/DeviceDoc/20001952C.pdf
+
+MCP23018 / MCP23S18 :
+http://ww1.microchip.com/downloads/en/DeviceDoc/22103a.pdf
+*/
+package mcp23xxx
