eb9f63246e3a8416663411a2b5cc19663755102a
Arnaud Rocher <arnaud.roche3@gmail.com>
2019-11-17 21:02:44 +0100

    Remove repetitive configuration step

--- devices/mcp23xxx/features.go
+++ devices/mcp23xxx/features.go
@@ -22,18 +22,17 @@ type register byte
 
 const (
 	// Register addresses.
-	rIODIR      register = 0x00
-	rIPOL       register = 0x01
-	rGPINTEN    register = 0x02
-	rDEFVAL     register = 0x03
-	rINTCON     register = 0x04
-	rIOCON      register = 0x05
-	rGPPU       register = 0x06
-	rINTF       register = 0x07
-	rINTCAP     register = 0x08
-	rGPIO       register = 0x09
-	rOLAT       register = 0x0A
-	rIOCONBANK0 byte     = 0x0B
+	rIODIR   register = 0x00
+	rIPOL    register = 0x01
+	rGPINTEN register = 0x02
+	rDEFVAL  register = 0x03
+	rINTCON  register = 0x04
+	rIOCON   register = 0x05
+	rGPPU    register = 0x06
+	rINTF    register = 0x07
+	rINTCAP  register = 0x08
+	rGPIO    register = 0x09
+	rOLAT    register = 0x0A
 )
 
 func (r register) addr(bankB bool) byte {
